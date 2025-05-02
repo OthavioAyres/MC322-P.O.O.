9740231b3af858f1d0f636ce4019837b68cf6ae1
OthavioAyres <othavio.ayres@neuralmind.ai>
2025-05-02 16:03:17 -0300

    Implementa a interface Comparable na classe Cliente e modifica o tratamento de email, agora utilizando um objeto Email. Adiciona notificações para compra e cancelamento de ingressos, além de métodos para manipulação do endereço de email.

--- Lab02/app/src/main/java/lab02/Cliente.java
+++ Lab02/app/src/main/java/lab02/Cliente.java
@@ -8,20 +8,20 @@ import java.util.ArrayList;
 import lab02.exceptions.IngressoNaoEncontradoException;
 import lab02.exceptions.CancelamentoNaoPermitidoException;
 
-public class Cliente {
+public class Cliente implements Comparable<Cliente> {
 
     private String nome;
-    private String email;
+    private Email email;
     private List<Ingresso> ingressos;
 
     /**
      * Construtor da classe cliente
      * @param nome o nome do cliente
-     * @param email o email do cliente
+     * @param enderecoEmail o endereço de email do cliente
      */
-    public Cliente(String nome, String email){
+    public Cliente(String nome, String enderecoEmail){
         this.nome = nome;
-        this.email = email;
+        this.email = new Email(enderecoEmail);
         this.ingressos = new ArrayList<>();
     }
 
@@ -42,19 +42,27 @@ public class Cliente {
     }
 
     /**
-     * Retorna o email do cliente
-     * @return o email do cliente
+     * Retorna o objeto email do cliente
+     * @return o objeto Email do cliente
      */
-    public String getEmail(){
+    public Email getEmail(){
         return email;
     }
 
     /**
-     * Altera o email do cliente para `email` 
-     * @param email o novo email do cliente
+     * Retorna o endereço de email do cliente
+     * @return o endereço de email do cliente
      */
-    public void setEmail(String email){
-        this.email = email;
+    public String getEnderecoEmail(){
+        return email.getEndereco();
+    }
+
+    /**
+     * Altera o email do cliente para `enderecoEmail` 
+     * @param enderecoEmail o novo endereço de email do cliente
+     */
+    public void setEnderecoEmail(String enderecoEmail){
+        this.email.setEndereco(enderecoEmail);
     }
 
     /**
@@ -71,6 +79,12 @@ public class Cliente {
      */ 
     public void adicionarIngresso(Ingresso ingresso){
         ingressos.add(ingresso);
+        // Notifica o cliente sobre a compra do ingresso
+        String assunto = "Confirmação de compra de ingresso";
+        String mensagem = "Sua compra para o evento '" + ingresso.getEvento().getNome() + 
+                         "' no dia " + ingresso.getEvento().getData() + 
+                         " foi confirmada com sucesso!";
+        email.exibirNotificacao(assunto, mensagem);
     }
 
     /**
@@ -80,6 +94,12 @@ public class Cliente {
     public void adicionarIngresso(List<Ingresso> novosIngressos){
         for (Ingresso ingresso : novosIngressos) {
             ingressos.add(ingresso);
+            // Notifica o cliente sobre a compra de cada ingresso
+            String assunto = "Confirmação de compra de ingresso";
+            String mensagem = "Sua compra para o evento '" + ingresso.getEvento().getNome() + 
+                         "' no dia " + ingresso.getEvento().getData() + 
+                         " foi confirmada com sucesso!";
+            email.exibirNotificacao(assunto, mensagem);
         }
     }
 
@@ -118,5 +138,32 @@ public class Cliente {
 
         // Remove o ingresso da lista
         ingressos.remove(ingressoParaCancelar);
+        
+        String assunto = "Cancelamento de ingresso";
+        String mensagem = "Seu ingresso para o evento '" + evento.getNome() + 
+                         "' no dia " + evento.getData() + 
+                         " foi cancelado com sucesso!";
+        email.exibirNotificacao(assunto, mensagem);
+    }
+    
+    /**
+     * Compara se este cliente possui ingressos para o mesmo evento que outro cliente
+     * @param outroCliente o cliente a ser comparado
+     * @return 0 se ambos possuem ingressos para pelo menos um evento em comum,
+     *         1 se não possuem ingressos para eventos em comum
+     */
+    @Override
+    public int compareTo(Cliente outroCliente) {
+        // Verificar se os clientes possuem ingressos para os mesmos eventos
+        for (Ingresso meuIngresso : this.ingressos) {
+            for (Ingresso outroIngresso : outroCliente.getIngressos()) {
+                // Se encontrarmos pelo menos um evento em comum, retorna 0 (iguais)
+                if (meuIngresso.getEvento().equals(outroIngresso.getEvento())) {
+                    return 0;
+                }
+            }
+        }
+        // Se não encontrou eventos em comum, retorna 1 (diferentes)
+        return 1;
     }
 }
